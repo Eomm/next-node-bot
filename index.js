b5dc70255d9bf5d97fac780224e9788a0b9fddf3
Manuel Spigolon <behemoth89@gmail.com>
2019-01-28 23:11:59 +0100

    fix: cloning and add walking path for js files

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const Octokit = require('@octokit/rest')
 const { spawn } = require('child_process')
-const { join } = require('path')
+const path = require('path')
+const fs = require('fs')
 
 const util = require('util')
 const setTimeoutPromise = util.promisify(setTimeout)
@@ -15,8 +16,8 @@ const githubClient = new Octokit({
 
 // a repo with old constructor new Buffer
 const repo = {
-  owner: 'ZECTBynmo',
-  repo: 'tacnode'
+  owner: 'soldair',
+  repo: 'node-buffer-indexof'
   //  organization:
 }
 
@@ -25,8 +26,9 @@ fork(githubClient, repo)
     console.log(JSON.stringify(response, null, 2))
     return waitFork(response.data)
   })
-  .then((data) => clone(data.git_url, config.cloneDirectory))
-  .then((data) => updateSourceCode(join(config.cloneDirectory, data.name)))
+  .then((data) => clone(data.git_url, `${config.cloneDirectory}${data.name}`)
+    .then(() => data))
+  .then((data) => updateSourceCode(path.join(config.cloneDirectory, data.name)))
   .then(() => console.log('Completed'))
   .catch(err => console.log('Somethink went wrong', err))
 
@@ -42,9 +44,11 @@ function waitFork (gitData) {
   return new Promise((resolve, reject) => {
     let tentative = 0
     const checkStatus = () => {
+      console.log(gitData.url)
       return githubClient.request(`HEAD ${gitData.url}`)
         .catch(err => {
           if (tentative < maxRetry) {
+            console.log('Retry', tentative)
             tentative++
             return setTimeoutPromise(retryAfter)
               .then(() => checkStatus())
@@ -54,12 +58,13 @@ function waitFork (gitData) {
     }
 
     checkStatus()
-      .then(resolve)
+      .then(() => resolve(gitData))
       .catch(reject)
   })
 }
 
 function clone (gitUrl, to) {
+  console.log('clone', gitUrl, to)
   return new Promise((resolve, reject) => {
     const gitCommand = spawn('git', ['clone', gitUrl, to])
     gitCommand.stdout.setEncoding('utf8')
@@ -76,6 +81,26 @@ function clone (gitUrl, to) {
 }
 
 function updateSourceCode (directory) {
-  console.log(directory)
-  // TODO search for .js file to update
+  console.log('walking', directory)
+  for (const file of walkSync(directory, (path) => path.endsWith('.js'))) {
+    // TODO pipeline
+    console.info(file)
+  }
+}
+
+// https://gist.github.com/luciopaiva/4ba78a124704007c702d0293e7ff58dd
+function * walkSync (dir, filter) {
+  const files = fs.readdirSync(dir)
+
+  for (const file of files) {
+    const pathToFile = path.join(dir, file)
+    const isDirectory = fs.statSync(pathToFile).isDirectory()
+    if (isDirectory) {
+      yield * walkSync(pathToFile, filter)
+    } else {
+      if (filter === undefined || filter(pathToFile) === true) {
+        yield pathToFile
+      }
+    }
+  }
 }
